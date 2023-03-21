b9cb6abe0373d3b123f1b7156e7acdd8af815f9c
Kennyveoba <kennyveoba@gmail.com>
2023-03-21 03:42:27 -0600

    Inicio de sesión

---
+++ Web/registroInstitucion.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php
+            include "includes/Encabezado.php";
+    ?>
+
+
+    <body>
+
+        
+        <div class="espacio">   
+        </div>
+
+        <div class="contenedor">
+        </div>
+        
+        <div class="espacio">       
+        </div>
+
+        <?php
+            include "includes/PiePagina.php";
+        ?>
+
+
+    </body> 
+        
+    <script type="text/javascript">
+        if (window.history.replaceState) {
+            window.history.replaceState(null, null, window.location.href);
+        }
+
+    </script>
+</html>
