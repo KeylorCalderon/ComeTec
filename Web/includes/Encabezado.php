f66a9c91ff44f4f4403164b5bf8e292d0cb8671a
KeylorCalderon <43432543+KeylorCalderon@users.noreply.github.com>
2023-03-20 21:47:10 -0600

    Cambios menores

--- Web/includes/Encabezado.php
+++ Web/includes/Encabezado.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Come-TEC</title>
-    <link rel="stylesheet" href="estilos.css" />
+    <link rel="stylesheet" href="css/estilos.css" />
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -12,7 +12,7 @@
 </head>
 
 <header>
-    <a href="index.php"><img width="200px" src="img/logo.jpg" alt="logo"></a>
+    <a href="index.php"><img width="200px" src="images/Logo.png" alt="logo"></a>
 
         <div class="buscador">
                 <input type="text" name="search" placeholder="Buscar..." class="src" autocomplete="off">
---
+++ Web/ranking.php
@@ -0,0 +1,31 @@
+<?php
+        include "includes/sesionInicio.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<?php
+        include "includes/Encabezado.php";
+?>
+<body>
+    <div class="wrapper">        
+        <main>
+            <nav class="menu-paginas">
+                <a href="catalogo-productos-servicios.php">Catalogo de productos</a>
+                <a href="ReiniciarBD.php">Reiniciar la Base de Datos</a>
+                <a href="automatizacion.php">Registrar empresa</a>
+                <a href="facturasAutomaticas.php">Generación de facturas</a>
+            </nav> 
+        </main>
+
+    </div>
+
+    <?php
+        include "includes/PiePagina.php";
+    ?>
+</body>
+
+</html>
